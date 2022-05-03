458d2ba914390b95e0cb055b036365dff95c71cd
Uttamdp10 <90449933+Uttamdp10@users.noreply.github.com>
2022-05-03 22:48:46 +0530

    Time: 9 ms (45.55%), Space: 6.3 MB (81.49%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
    int strStr(string haystack, string needle) 
     {
-        if(needle=="")
-            return 0;
+       
         
         int i=0,j=0;
         while(i<haystack.size() && j<needle.size())
