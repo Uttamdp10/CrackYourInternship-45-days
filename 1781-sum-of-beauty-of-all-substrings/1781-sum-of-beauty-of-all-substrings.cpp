4dc1aebd3a2bf27621f16f8b33b7119bd9fdccdd
Uttamdp10 <90449933+Uttamdp10@users.noreply.github.com>
2022-05-05 19:50:11 +0530

    Time: 592 ms (35.19%), Space: 26.7 MB (20.06%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
     int beautySum(string s) {
-        int sum=0;
-        
-        for(int i=0;i<s.length();i++)
-        {
-            string str="";
-            int arr[26]={0};
-            for(int j=i;j<s.length();j++)
-            {
-                str+=s[j];
-                arr[s[j]-'a']++;
-
-                int maxi=0;int mini=INT_MAX;
-                for(int i=0;i<26;i++)
-                {
-                    if(arr[i]!=0)
-                    {
-                    maxi=max(maxi,arr[i]);
-                    mini=min(mini,arr[i]);
-                
-                    }
-                }
-                if(mini==INT_MAX)
-                    mini=0;
-                sum+=(maxi-mini);
-                
-            }
-        }
-        return sum;
+       int sumofdifferences=0;
+        int n=s.length();
+         map<char,int> freq;
+        for(int i=0;i<n;i++)
+       {
+           freq.clear();
+           for(int j=i;j<n;j++)
+           {
+               int maxi=INT_MIN,mini=INT_MAX;
+               freq[s[j]]++;
+               if(freq.size()==1||freq.size()==j-i+1)
+               {
+                   continue;
+               }
+               else
+               {
+                   for(auto each:freq)
+                   {
+                       if(each.second>maxi)
+                       maxi=each.second;
+                       if(each.second<mini)
+                       mini=each.second;
+                   }
+                   sumofdifferences+=(maxi-mini);
+               }
+           }
+       }
+       return sumofdifferences;
     }
 };
