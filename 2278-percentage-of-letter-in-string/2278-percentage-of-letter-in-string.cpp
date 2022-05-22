f37c6aacdb840df840c6ea6ec959e9a86e18bb52
Uttamdp10 <90449933+Uttamdp10@users.noreply.github.com>
2022-05-22 16:29:28 +0530

    Time: 7 ms (25.00%), Space: 6.2 MB (25.00%) - LeetHub

--- 2278-percentage-of-letter-in-string/2278-percentage-of-letter-in-string.cpp
+++ 2278-percentage-of-letter-in-string/2278-percentage-of-letter-in-string.cpp
@@ -1,19 +1,8 @@
 class Solution {
 public:
-    int percentageLetter(string s, char letter)
-    {
-        int n = s.size();
-        int ans = 0, val =0;
-        
-         for(int i = 0; i < n; i++)
-         {
-                 if(s[i] == letter)
-                         ans++;
-                 
-                 
-         }
-            ans = ans * 100;
-            return ans/n;
+    int percentageLetter(string s, char letter) {
+		return 100 * count(begin(s), end(s), letter) / s.size();
+	
           
     }
 };
