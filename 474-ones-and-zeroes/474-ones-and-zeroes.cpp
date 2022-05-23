fd8f17fcf74707ad9b57a872ca3c600325843c93
Uttamdp10 <90449933+Uttamdp10@users.noreply.github.com>
2022-05-23 10:50:45 +0530

    Time: 91 ms (97.04%), Space: 8.6 MB (98.79%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
-    int findMaxForm(vector<string>& strs, int m, int n) {
+    int findMaxForm(vector<string>& strs, int m, int n) 
+    {
         int dp[101][101]{};
-        for(int i = 0; i < strs.size(); i++){
+        for(int i = 0; i < strs.size(); i++)
+        {
             int cnt_0 = count(strs[i]), cnt_1 = strs[i].size() - cnt_0;
-            for(int m_quota = m; m_quota >= 0; m_quota--)
-                for(int n_quota = n; n_quota >= 0; n_quota--)
-                    if(cnt_0 <= m_quota and cnt_1 <= n_quota)
-                        dp[m_quota][n_quota] = max(dp[m_quota][n_quota], dp[m_quota-cnt_0][n_quota-cnt_1] + 1);
+            for(int m_q = m; m_q >= 0; m_q--)
+                for(int n_q = n; n_q >= 0; n_q--)
+                    if(cnt_0 <= m_q and cnt_1 <= n_q)
+                        dp[m_q][n_q] = max(dp[m_q][n_q], dp[m_q-cnt_0][n_q-cnt_1] + 1);
         }
         return dp[m][n];
     }
     
-    int count(string &s){
+    int count(string &s)
+    {
         int cnt_0{0};
         for(char c: s)
             cnt_0+= (c == '0');
