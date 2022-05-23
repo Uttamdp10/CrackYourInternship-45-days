abfc616e8c4982ff06ec579c21c3977220149b07
Uttamdp10 <90449933+Uttamdp10@users.noreply.github.com>
2022-05-23 10:55:51 +0530

    Time: 85 ms (97.42%), Space: 8.6 MB (99.95%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -3,12 +3,19 @@ public:
     int findMaxForm(vector<string>& strs, int m, int n) 
     {
         int dp[101][101]{};
+                            // m is the number of ones in array
+                            // n is the number of zeros in array
+            
         for(int i = 0; i < strs.size(); i++)
         {
             int cnt_0 = count(strs[i]), cnt_1 = strs[i].size() - cnt_0;
+                
             for(int m_q = m; m_q >= 0; m_q--)
+                    
                 for(int n_q = n; n_q >= 0; n_q--)
+                        
                     if(cnt_0 <= m_q and cnt_1 <= n_q)
+                            
                         dp[m_q][n_q] = max(dp[m_q][n_q], dp[m_q-cnt_0][n_q-cnt_1] + 1);
         }
         return dp[m][n];
@@ -17,8 +24,11 @@ public:
     int count(string &s)
     {
         int cnt_0{0};
-        for(char c: s)
-            cnt_0+= (c == '0');
+        for(int i = 0; i < s.size(); i++)
+        {
+                if(s[i] == '0')
+                        cnt_0++;
+        }  
         return cnt_0;
     }
 };
