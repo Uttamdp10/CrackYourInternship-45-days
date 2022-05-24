680958679dbd7385fc11247d8c38a46787216197
Uttamdp10 <90449933+Uttamdp10@users.noreply.github.com>
2022-05-24 10:07:18 +0530

    Time: 13 ms (12.88%), Space: 7.3 MB (42.55%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
-    int longestValidParentheses(string s) 
-    {
-        stack<int> stk;
-        stk.push(-1);   
-        int maxlength = 0;
-        for(int i = 0; i < s.size(); i++)
-        {
-             int t = stk.top();
-             
-                // t != -1 checks the condition that stack is empty or not and the index i and top of stack matches a valid paranthesis then pop out.
-                
-                if(t != -1 && s[i] == ')' && s[t] == '(')
-                {
-                        stk.pop();
-                        // maxlength is calculated from the top to the index up to which the condition is satisfied for valid paranthesis i.e ().
-                        maxlength = max(maxlength, i-stk.top());
-                }      
-                else
-                        stk.push(i);
-        }
-            return maxlength;
-    }
+    
+       int longestValidParentheses(string s) {
+	int MAX = 0;  // denotes length of maximum valid parentheses
+	stack<int> stk;
+	stk.push(-1); // bottom of stack will always hold index preceding to potential start of valid parentheses
+	for(int i = 0; i < size(s); i++)
+		if(s[i] == '(') stk.push(i);            
+		else{                
+			stk.pop();
+			if(stk.empty()) stk.push(i);
+			else MAX = max(MAX, i - stk.top());
+		}        
+	return MAX;
+}
+    
         
         
 };
