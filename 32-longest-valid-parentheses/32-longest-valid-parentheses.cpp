7217c756de319d709a504280a909f8eb97159db9
Uttamdp10 <90449933+Uttamdp10@users.noreply.github.com>
2022-05-24 10:00:11 +0530

    Time: 3 ms (88.93%), Space: 7.4 MB (20.23%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -9,9 +9,12 @@ public:
         {
              int t = stk.top();
              
+                // t != -1 checks the condition that stack is empty or not and the index i and top of stack matches a valid paranthesis then pop out.
+                
                 if(t != -1 && s[i] == ')' && s[t] == '(')
                 {
                         stk.pop();
+                        // maxlength is calculated from the top to the index up to which the condition is satisfied for valid paranthesis i.e ().
                         maxlength = max(maxlength, i-stk.top());
                 }      
                 else
