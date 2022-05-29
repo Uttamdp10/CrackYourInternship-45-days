b146a0b996865c39d25b7e09985def43e8c1e5cf
Uttamdp10 <90449933+Uttamdp10@users.noreply.github.com>
2022-05-29 10:58:05 +0530

    Time: 189 ms (28.54%), Space: 15.7 MB (64.52%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -1,31 +1,22 @@
 class Solution {
 public:
-    bool check(string a,string b)
-    {
-        for(int i=0;i<a.size();i++)
-        {
-            for(int j=0;j<b.size();j++)
-            {
-                if(a[i]==b[j])
-                    return false;
-            }
-        }
-        return true;
-    }
-    int maxProduct(vector<string>& words) {
-        int ma=0;
-        for(int i=0;i<words.size();i++)
-        {
-            for(int j=i+1;j<words.size();j++)
-            {
-                 if(words[i].size() * words[j].size() <= ma) continue;
-                if(check(words[i],words[j]))
-                {
-                    int l=words[i].size()*words[j].size();
-                    ma=max(ma,l);
-                }
-            }
-        }
-        return ma;
-    }
+   int maxProduct(vector<string>& words) {
+	int n = size(words), ans = 0;
+	vector<bitset<26> > chars(n);
+	for(int i = 0; i < n; i++) {
+		for(auto& ch : words[i])  // map the letters to 1 if it occurs in the word
+			chars[i][ch - 'a'] = 1;
+		// now check with all other words and if there's no common letter, then update ans	
+		for(int j = 0; j < i; j++)
+			if(!checkCommon(chars[i], chars[j])) 
+				ans = max(ans, int(size(words[i]) * size(words[j])));
+	}   
+	return ans;
+}
+// Returns true if there's a common letter between bitset of two words
+bool checkCommon(bitset<26>& a, bitset<26>& b) {
+	for(int i = 0; i < 26; i++) if (a[i] && b[i]) return true;
+	return false;
+}
+
 };
