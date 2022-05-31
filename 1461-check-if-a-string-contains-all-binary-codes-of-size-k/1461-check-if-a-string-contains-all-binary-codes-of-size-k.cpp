f90c3e287f45572c02860b54dd0f4d6e73db090e
Uttamdp10 <90449933+Uttamdp10@users.noreply.github.com>
2022-05-31 09:02:21 +0530

    Time: 38 ms (99.37%), Space: 18.5 MB (94.34%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     bool hasAllCodes(string s, int k) {
-        if (k > s.size()) return false;
-        
-        unordered_set<string> my_set;
-        
-        for (int i = 0; i <= s.size()-k; i++)
-            my_set.insert(s.substr(i, k));
-        
-        return my_set.size() == pow(2, k);
-    }
+	int n = s.size(), size = 1 << k, count = 0, hash = 0, allOnes = ~size; 
+	bool set[size]; memset(set, false, sizeof(set));
+	for(int i = 0; i < n; i++){                    	    
+		// below line combines all above-metioned steps for rolling hash calculation
+		hash = ((hash << 1) & allOnes) | (s[i] - '0');            
+		if(i >= k - 1 && !set[hash])   // first k - 1 iteration are to calculate hash of first substring of length k
+			set[hash] = true, count++;
+	}
+	return count == size;
+}
 };
