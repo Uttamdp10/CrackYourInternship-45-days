b067741463798c385763afcac195a56c53c6ce98
Uttamdp10 <90449933+Uttamdp10@users.noreply.github.com>
2022-06-01 09:43:01 +0530

    Time: 3 ms (76.46%), Space: 8.6 MB (38.06%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
-    // Optimal Solution Time O(N) & Auxiliary Space O(1)
-    int len=nums.size();
-    for(int i=1;i<len;i++){
-        nums[i]+=nums[i-1];
-    }
-        return nums;  
+        vector<int> ans;
+        int n=nums.size();
+        int count =0;
+        
+        for(int i=0; i<n; i++){
+            count += nums[i];
+            ans.push_back(count);
+        }
+        return ans;
     }
 };
