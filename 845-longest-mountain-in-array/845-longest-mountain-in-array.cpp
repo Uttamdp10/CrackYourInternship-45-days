9227b1c2c5aa6b58b5ba4ca7de4badd548f5a573
Uttamdp10 <90449933+Uttamdp10@users.noreply.github.com>
2022-06-05 15:20:41 +0530

    Time: 30 ms (43.47%), Space: 18.5 MB (84.93%) - LeetHub

--- 845-longest-mountain-in-array/845-longest-mountain-in-array.cpp
+++ 845-longest-mountain-in-array/845-longest-mountain-in-array.cpp
@@ -9,18 +9,22 @@ public:
             for(int i = 1; i < a.size()-1; i++)
             {
                     if(a[i] > a[i-1] && a[i] > a[i+1])
+                            //finding out the middle element or peak in an array/mountain.
                     {      
-                            int left = i-1;
+                            int left = i-1; //initialize left pointer towards left of peak 
                      
-                            int right = i+1;
+                            int right = i+1; // initialize right pointer towards right to peak.
                             
                             while(left > 0 && a[left-1] < a[left] )
+                                    // if next left is smaller then current left move forward;
                                     left--;
                             
                             while(right < a.size()-1 && a[right+1] < a[right])
+                                    // if next right is smaller then current right move forward
                                     right++;
                             
                             maxlength = max(maxlength, right - left+1);
+                            //return the maximum length between two pointers right and left.
                     }
             }
             
