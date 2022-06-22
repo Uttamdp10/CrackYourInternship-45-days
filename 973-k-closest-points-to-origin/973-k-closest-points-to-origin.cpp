cb2807f26f24808f8b6ddb5a0766b6cd278d7f59
Uttamdp10 <90449933+Uttamdp10@users.noreply.github.com>
2022-06-22 19:46:23 +0530

    Time: 429 ms (37.29%), Space: 71.6 MB (39.65%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -2,20 +2,23 @@ class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k)
     {
-       vector<pair<int,int>> sde;
-       vector<vector<int>>v;
-            
-           for(int i = 0; i < points.size(); i++)
-           {
-                   int d = points[i][0]*points[i][0] + points[i][1] * points[i][1];
-                   sde.push_back({d,i});
-           }
-           sort(sde.begin(), sde.end()); 
-            for(int i=0;i<k;i++)
-        {
-            v.push_back(points[sde[i].second]);
+        //Answer vector
+        vector<vector<int>> result(k);
+        //maxheap storage initialization
+        priority_queue<vector<int>> maxHeap;
+         for (auto& p : points) {
+            int x = p[0], y = p[1];
+            maxHeap.push({x*x + y*y, x, y});
+            if (maxHeap.size() > k) {
+                maxHeap.pop();
+            }
+        }
+        for (int i = 0; i < k; ++i) {
+            vector<int> top = maxHeap.top();
+            maxHeap.pop();
+            result[i] = {top[1], top[2]};
         }
         
-        return v;
+        return result;    
     }
 };
