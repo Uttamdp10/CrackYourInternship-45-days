2718574d31b236b99980a4a7b850c4ca137ffc35
Uttamdp10 <90449933+Uttamdp10@users.noreply.github.com>
2022-06-24 21:13:15 +0530

    Time: 16 ms (28.76%), Space: 9.6 MB (74.10%) - LeetHub

--- 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.cpp
+++ 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.cpp
@@ -1,39 +1,30 @@
-typedef pair<int,int> p;
 class Solution {
 public:
     vector<int> maxSubsequence(vector<int>& nums, int k) {
-        // Heap 'q' will store the k largest elements of the array 
-        // The following heap is of pair where first element of pair is the element and the second element is the corresponding index
-        // That's how in queue sorting will be done according to the array element
-        priority_queue<p,vector<p>,greater<p>>q;
-        for(int i=0;i<nums.size();i++)
-        {
-            q.push({nums[i],i});
-            if(q.size()>k)
-            {
-                q.pop();
+        //Using a Priority Queue to store pair
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        for(int i = 0 ; i <nums.size(); i++){
+            pq.push({nums[i], i});
+            if(pq.size()>k){
+                pq.pop();
             }
         }
-        
-        // heap 'aux' will sort the elements based on their index
-        // sorting according to the index is necessary in order get the original order of the elements 
-		// in the following heap the first element of the pair is the index and the second element is the array element itself
-        priority_queue<p,vector<p>,greater<p>>aux;
-        while(!q.empty())
-        {
-            aux.push({q.top().second,q.top().first});
-            q.pop();
+        // Store the k elements 
+        vector<pair<int, int>> ans;
+        while(!pq.empty()){
+            auto x = pq.top();
+            pq.pop();
+            ans.push_back({x.second , x.first});
         }
         
-        // once the elements are sorted according to their indices , push them in a vector
-        vector<int>ans;
-        while(!aux.empty())
-        {
-            ans.push_back(aux.top().second);
-            aux.pop();
-        }
+        //Sort k element based on original index
+        sort(ans.begin(), ans.end());
         
-        // finally return the answer
-        return ans;
+        //Return the ans
+        vector<int> finalAns;
+        for(auto x : ans){
+            finalAns.push_back(x.second);
+        }
+        return finalAns;
     }
 };
