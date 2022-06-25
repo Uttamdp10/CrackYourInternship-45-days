40db782d698852d5d1c856bf5545563ead085473
Uttamdp10 <90449933+Uttamdp10@users.noreply.github.com>
2022-06-25 18:00:59 +0530

    Time: 187 ms (63.52%), Space: 90 MB (25.07%) - LeetHub

--- 2100-find-good-days-to-rob-the-bank/2100-find-good-days-to-rob-the-bank.cpp
+++ 2100-find-good-days-to-rob-the-bank/2100-find-good-days-to-rob-the-bank.cpp
@@ -1,27 +1,14 @@
 class Solution {
 public:
-    vector<int> goodDaysToRobBank(vector<int> &a, int time) {
-        int n=a.size();
-        
-        vector<int> left(n,0), right(n,0);
-        
-        for(int i=1;i<n;i++){
-            if(a[i]<=a[i-1]){
-                left[i]=1+left[i-1]; // count of elements from [0,i-1] >= a[i]
+    bool checkPossibility(vector<int>& nums) {
+        int cnt = 0;             
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i] < nums[i-1]){
+                if(++cnt > 1) return false;
+                if(i == 1 || nums[i-2] <= nums[i]) nums[i-1] = nums[i];    
+               
             }
         }
-        
-        for(int i=n-2;i>=0;i--){
-            if(a[i]<=a[i+1]){
-                right[i]=1+right[i+1]; // count of elements from [i+1,n-1] >= a[i]
-            }
-        }
-        
-        vector<int> ans;
-        for(int i=time;i<n-time;i++){
-            if(left[i]>=time && right[i]>=time) ans.push_back(i);
-        }
-        return ans;
-    }
+        return true;
+    } 
 };
-
