76d951c8648ff0dd8ec2249b7c68197e4bdbb43a
Uttamdp10 <90449933+Uttamdp10@users.noreply.github.com>
2022-06-26 14:18:03 +0530

    Time: 100 ms (34.34%), Space: 42.3 MB (80.56%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,17 +1,22 @@
+//OPTIMAL APPROACH FOR THIS QUESTION ..........T.C == O(2K)=>O(K) AND S.C == O(1)
 class Solution {
 public:
-    int maxScore(vector<int>& nums, int k) {
-        int front=k-1,back=nums.size()-1;
-        int sum=0,maxi=0;
-        for(int i=0;i<k;i++){
-            sum+=nums[i];
+    int maxScore(vector<int>& cardPoints, int k) {
+        int res = 0;
+		
+		//First k elements in our window
+        for(int i=0;i<k;i++) res+=cardPoints[i];
+        
+        int curr=res;
+        for(int i=k-1;i>=0;i--) {
+			//We remove the last visited element and add the non-visited element from the last
+            curr-=cardPoints[i];
+            curr+=cardPoints[cardPoints.size()-k+i];
+			
+            //We check the maximum value any possible combination can give
+			res = max(res, curr);
         }
-        maxi=sum;
-        while(front>=0 && front<back){
-            sum+=nums[back]-nums[front];
-            maxi=max(maxi,sum);
-            back--; front--;
-        }
-        return maxi;
+        
+        return res;
     }
 };
