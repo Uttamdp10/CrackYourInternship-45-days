12a42f2dc2cd8ba98b3ad2a9677539cc625cd93b
Uttamdp10 <90449933+Uttamdp10@users.noreply.github.com>
2022-06-28 18:46:03 +0530

    Time: 153 ms (24.71%), Space: 17.4 MB (37.46%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
-    int minDeletions(string s) 
-    {
-            vector<int>freq(26,0);
-            
-            for(auto c : s)
+    int minDeletions(string s) {
+        unordered_map<int,int>charcnt;
+        unordered_map<int,int>uniquefreq;
+        int count=0;
+        
+        for(int i=0;i<s.size();i++)
+        {
+            charcnt[s[i]-'a']++;
+        }
+        
+        for(auto &it : charcnt)
+        {
+            int x= it.second;
+            if(uniquefreq[x]!=0)
             {
-                    freq[c - 'a']++;
+                while(x>0 && uniquefreq[x]!=0)
+                {
+                    x--;
+                    count++;
+                }
             }
-        sort(freq.begin(), freq.end());
-        int deletion = 0;
-         
-            for(int i = 24; i >= 0; i--)
+            if(x>0)
             {
-                if(freq[i] == 0)
-                        break;
-                  if(freq[i] >= freq[i+1])
-                  {
-                          int prev = freq[i];
-                          freq[i] = max(0 , freq[i+1] - 1);
-                          deletion += prev - freq[i];
-                  }
+                uniquefreq[x]++;
             }
-            
-            
-            return deletion;
+        }
+        
+        return count;
     }
 };
