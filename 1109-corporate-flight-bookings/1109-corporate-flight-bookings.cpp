859f7d1e2bcaf07b82a501e8bfd56bf946932fed
Uttamdp10 <90449933+Uttamdp10@users.noreply.github.com>
2022-07-21 23:38:27 +0530

    Time: 422 ms (20.60%), Space: 67.7 MB (89.33%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
-      static bool compare(vector<int> &a , vector<int> &b)
-    {
-        if(a[1]==b[1])
-        {
-            return a[0]<b[0];
-        }
-        return a[1]<b[1];
-    }
+      
     
     vector<int> corpFlightBookings(vector<vector<int>>& b , int n) {
         
