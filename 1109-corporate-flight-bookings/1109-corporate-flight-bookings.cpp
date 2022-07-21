0144e57b6f9764f03596fa365830896ab2c05ef5
Uttamdp10 <90449933+Uttamdp10@users.noreply.github.com>
2022-07-21 23:36:15 +0530

    Time: 409 ms (23.41%), Space: 67.8 MB (59.11%) - LeetHub

---
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+      static bool compare(vector<int> &a , vector<int> &b)
+    {
+        if(a[1]==b[1])
+        {
+            return a[0]<b[0];
+        }
+        return a[1]<b[1];
+    }
+    
+    vector<int> corpFlightBookings(vector<vector<int>>& b , int n) {
+        
+   //      sort(b.begin(),b.end(),compare);
+        
+        vector<int> res(n , 0);
+        
+         for(int i=0;i<b.size();i++)
+         {
+             int first_idx=b[i][0]-1;
+             int last_idx=b[i][1]-1;
+             
+             int value=b[i][2];
+             
+             res[first_idx]+=value;
+             
+             // now this value will be valid till last_idx 
+             
+             // not valid from last_idx+1
+             
+             if((last_idx+1)<n)
+             {
+                 res[last_idx+1]-=value;
+             }
+         }
+        
+        for(int i=1;i<n;i++)
+        {
+            res[i]+=res[i-1];
+        } 
+        
+        return res;
+        
+    
+    }
+};
